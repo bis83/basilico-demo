18d78350413b2a5828e85231739b4f77a925a6fd
bis <bis83gb@gmail.com>
2022-05-24 23:11:03 +0900

    add makeworld

--- js/action.js
+++ js/action.js
@@ -1,4 +1,19 @@
 
+$action["makeworld"] = () => {
+    $stack.w = 64;
+    $stack.h = 64;
+    $stack.a.length = 0;
+    $stack.a.length = $stack.w * $stack.h;
+    for(let i=0; i<$stack.a.length; ++i) {
+        const h = 1 + Math.floor(Math.random() * 10)
+        $stack.a[i] = [];
+        $stack.a[i].push(stack_set(1, h));
+    }
+
+    $pos.x = $stack.w/2 + 0.5;
+    $pos.y = $stack.h/2 + 0.5;
+};
+
 $action["pushstack"] = () => {
     const stack = stack_value($pos.x, $pos.y);
     if(stack && stack.length > 0) {
