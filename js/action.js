4796473e527154faaec6aa73f36ace1b725e4d98
bis <bis83gb@gmail.com>
2022-06-04 23:03:26 +0900

    fix makeworld

--- js/action.js
+++ js/action.js
@@ -1,14 +1,15 @@
 
 $action["makeworld"] = () => {
+    const no = data_tile_index("tile");
+
     $tile.w = 64;
     $tile.h = 64;
     $tile.a.length = 0;
     $tile.a.length = $tile.w * $tile.h;
     for(let i=0; i<$tile.a.length; ++i) {
         $tile.a[i] = [];
-        $tile.a[i].push(tile_set(1, 1));
+        $tile.a[i].push({no: no, count:1});
     }
-
     $pos.x = $tile.w/2 + 0.5;
     $pos.y = $tile.h/2 + 0.5;
 };
@@ -19,11 +20,11 @@ $action["pushtile"] = () => {
         return;
     }
     if(tile.length > 0) {
-        let [id, count] = tile_get(tile[tile.length-1]);
-        count += 1;
-        tile[tile.length-1] = tile_set(id, count);
+        const obj = tile[tile.length-1];
+        obj.count += 1;
     } else {
-        tile.push(tile_set(1,1));
+        const no = data_tile_index("tile");
+        tile.push({no: no, count:1});
     }
 };
 
@@ -33,11 +34,9 @@ $action["poptile"] = () => {
         return;
     }
     if(tile.length > 0) {
-        let [id, count] = tile_get(tile[tile.length-1]);
-        count -= 1;
-        if(count > 0) { 
-            tile[tile.length-1] = tile_set(id, count);
-        } else {
+        const obj = tile[tile.length-1];
+        obj.count -= 1;
+        if(obj.count <= 0) {
             tile.pop();
         }
     }
