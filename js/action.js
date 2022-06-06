c5e79bbfd8ec183d0e2916a4d7afd279acb16971
bis <bis83gb@gmail.com>
2022-06-06 22:56:11 +0900

    fix tile api

--- js/action.js
+++ js/action.js
@@ -2,42 +2,32 @@
 $action["makeworld"] = () => {
     const no = data_tile_index("tile");
 
-    $tile.w = 64;
-    $tile.h = 64;
-    $tile.a.length = 0;
-    $tile.a.length = $tile.w * $tile.h;
-    for(let i=0; i<$tile.a.length; ++i) {
-        $tile.a[i] = [];
-        $tile.a[i].push({no: no, count:1});
+    tile_init_empty(64, 64);
+    for(let x=24; x<=40; ++x) {
+        for(let y=24; y<=40; ++y) {
+            tile_base_set(x, y, no);
+        }
     }
+
     $pos.x = $tile.w/2 + 0.5;
     $pos.y = $tile.h/2 + 0.5;
 };
 
 $action["pushtile"] = () => {
-    const tile = tile_value($pos.x, $pos.y);
+    const tile = tile_base($pos.x, $pos.y);
     if(!tile) {
         return;
     }
-    if(tile.length > 0) {
-        const obj = tile[tile.length-1];
-        obj.count += 1;
-    } else {
-        const no = data_tile_index("tile");
-        tile.push({no: no, count:1});
-    }
+    tile.count += 1;
 };
 
 $action["poptile"] = () => {
-    const tile = tile_value($pos.x, $pos.y);
+    const tile = tile_base($pos.x, $pos.y);
     if(!tile) {
         return;
     }
-    if(tile.length > 0) {
-        const obj = tile[tile.length-1];
-        obj.count -= 1;
-        if(obj.count <= 0) {
-            tile.pop();
-        }
+    if(tile.count <= 1) {
+        return;
     }
+    tile.count -= 1;
 };
