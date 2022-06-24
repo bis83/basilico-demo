4b3963e437a5bea8bfc38d86dcfa4296f8f54055
bis <bis83gb@gmail.com>
2022-06-25 00:58:12 +0900

    add items

--- js/action.js
+++ js/action.js
@@ -3,20 +3,24 @@ $action["makeworld"] = () => {
     const b = data_tile_index("tile");
     const m = data_tile_index("mine");
 
+    // tile
     tile_init_empty(64, 64);
     for(let x=24; x<=40; ++x) {
         for(let y=24; y<=40; ++y) {
             tile_base_set(x, y, b);
         }
     }
-
     tile_prop_set(29, 29, m);
     tile_prop_set(35, 29, m);
     tile_prop_set(29, 35, m);
     tile_prop_set(35, 35, m);
 
-    $pos.x = $tile.w/2 + 0.5;
-    $pos.y = $tile.h/2 + 0.5;
+    // pos
+    pos_init($tile.w/2 + 0.5, $tile.h/2 + 0.5);
+
+    // item
+    item_init_empty(8);
+    item_gain(data_item_index("pick"), 1);
 };
 
 $action["pushtile"] = () => {
