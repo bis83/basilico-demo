238d583a6db069530f2df5c392a00207f70c400b
bis83 <bis83@users.noreply.github.com>
2023-02-17 17:04:22 +0000

    deploy: 4c59c9c67b58c441d074405db9085444a3ab0ff6

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-(()=>{})();
+(()=>{const o=(e,t,n)=>{e.addEventListener(t,n)};const s=()=>document.getElementById("message"),l=()=>{const e=s();e.style.display="none"};o(window,"load",()=>{l()});})();
