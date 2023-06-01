9b3021d06b67463c67297bbc0c59cd9a62d666ee
bis <bis83gb@gmail.com>
2023-06-01 09:19:34 +0900

    add setup function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 
-html_listen(window, "load", () => {
+const setup = (scene) => {
   html_hide_message();
-  basil3d_start();
+
+  scene.camera.eye = [0, 1.5, -5];
+  basil3d_scene_add_object(scene, "tr_01", mat4translate(-2, 0, 0));
+  basil3d_scene_add_object(scene, "tr_01", mat4translate(+2, 0, 0));
+  basil3d_scene_add_object(scene, "tr_01", mat4translate(0, 0, 4));
+};
+
+html_listen(window, "load", () => {
+  basil3d_start(setup);
 });
