f406e445d16fcfbba54bd6903bef1f607da80351
bis <bis83gb@gmail.com>
2023-06-03 15:00:44 +0900

    rename add_entity

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
-const setup = (scene) => {
+const setup = (app, scene) => {
   html_hide_message();
 
   scene.camera.eye = [0, 1.5, -5];
-  basil3d_scene_add_object(scene, "tr_01", mat4translate(-2, 0, 0));
-  basil3d_scene_add_object(scene, "tr_01", mat4translate(+2, 0, 0));
-  basil3d_scene_add_object(scene, "tr_01", mat4translate(0, 0, 4));
+  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(-2, 0, 0));
+  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(+2, 0, 0));
+  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(0, 0, 4));
 };
 
 html_listen(window, "load", () => {
