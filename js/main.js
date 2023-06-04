e32a409d08808546353946f99bc5aabba65f5ba6
bis <bis83gb@gmail.com>
2023-06-04 21:39:27 +0900

    fix scene_setup

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 const setup = (app, scene) => {
   html_hide_message();
 
-  scene.camera.eye = [0, 1.5, -5];
-  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(-2, 0, 0));
-  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(+2, 0, 0));
-  basil3d_scene_add_entity(scene, app, "tr_01", mat4translate(0, 0, 4));
+  basil3d_scene_setup(scene, app, {
+    camera: {
+      eye: [0, 1.5, -5],
+    },
+    entity: [
+      { name: "tr_01", matrix: mat4translate(-2, 0, 0) },
+      { name: "tr_01", matrix: mat4translate(+2, 0, 0) },
+      { name: "tr_01", matrix: mat4translate(0, 0, 4) },
+    ],
+  });
 };
 
 html_listen(window, "load", () => {
