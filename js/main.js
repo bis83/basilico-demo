08a8223b90cd23f9e832d9a9bb9a3fc9ec3806f5
bis <bis83gb@gmail.com>
2023-06-14 01:11:10 +0900

    rot camera

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const setup = (app, scene) => {
 
   basil3d_scene_setup(scene, app, {
     camera: {
-      eye: [0, 1.5, -5],
+      eye: [6.0, 2.5, -5.0],
+      dir: vec3dir(135, -10),
     },
     entity: [
       { name: "tr_01", matrix: mat4translate(-2, 0, 0) },
