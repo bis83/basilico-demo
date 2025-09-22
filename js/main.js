d7098477aacd9dcfde0c8f7239db4b947ef985ca
bis <bis83gb@gmail.com>
2025-09-22 23:03:47 +0900

    update api

--- js/main.js
+++ js/main.js
@@ -4,21 +4,50 @@ html_listen(window, "load", () => {
 });
 
 const update = () => {
-  $submitCamera(0, 2, -5, 90, -20, 35, 0.01, 1000);
-  $submitLightDirectional(0, 65, [0.8, 0.8, 0.8, 1.0]);
-  $submitLightAmbient([0.4, 0.4, 0.9, 0.8], [0.5, 0.4, 0.1, 0.4]);
+  {
+    const camera = $newCamera();
+    $cameraPosition(camera, 0, 2, -5);
+    $cameraAngle(camera, 90, -10);
+    $cameraFov(camera, 35);
+    $cameraZClip(camera, 0.01, 1000);
+    const light = $newLight();
+    $lightDirection(light, 0, 65);
+    $lightColor(light, 0.8, 0.8, 0.8, 1.0);
+    $lightAmbient0(light, 0.4, 0.4, 0.9, 0.8);
+    $lightAmbient1(light, 0.5, 0.4, 0.1, 0.4);
+    $writeStage($packStage(camera, light));
+  }
+  {
+    const lst = [];
+    const m = $newMesh();
 
-  $submitMesh("tr_01", [
-    {
-      x: 0,
-      y: 0,
-      z: 0,
-      ha: 0,
-      va: 0,
-      factor0: [1, 1, 1, 1],
-      factor1: [1, 0, 0, 0],
-      factor2: [0, 0, 0, 0],
-      factor3: [0, 0, 0, 0],
-    }
-  ]);
+    $meshPosition(m, -2, 0, 0);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 0, 0, 0);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 2, 0, 0);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, -2, 0, -2);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 0, 0, -2);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 2, 0, -2);
+    lst.push($writeMesh($packMesh(m)));
+
+    $draw("tr_01", lst);
+  }
+  {
+    const lst = [];
+    const m = $newMesh();
+    $meshAngle(m, 90, 0);
+
+    $meshPosition(m, -2, 0, 2);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 0, 0, 2);
+    lst.push($writeMesh($packMesh(m)));
+    $meshPosition(m, 2, 0, 2);
+    lst.push($writeMesh($packMesh(m)));
+
+    $draw("wa_00", lst);
+  }
 };
